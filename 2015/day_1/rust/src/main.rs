3911b6054463e774e413c9083d6ecc23caf9ded5
Hafsteinn Ragnarsson <haffi921@gmail.com>
2024-11-30 18:14:30 +0100

    Day 1 Part 2 of 2015 done

--- 2015/day_1/rust/src/main.rs
+++ 2015/day_1/rust/src/main.rs
@@ -1,14 +1,54 @@
 use std::fs;
 
-fn main() {
-    let input = fs::read_to_string("../input_1.txt").unwrap();
-    let mut floor = 0;
-    for c in input.chars() {
-        if c == '(' {
-            floor += 1;
-        } else if c == ')' {
-            floor -= 1;
+struct FloorIterator<'a> {
+    input: &'a str,
+    index: usize,
+    floor: i32,
+}
+
+impl<'a> FloorIterator<'a> {
+    fn new(input: &'a str) -> Self {
+        Self {
+            input,
+            index: 0,
+            floor: 0,
         }
     }
-    println!("Floor: {}", floor);
+}
+
+impl<'a> Iterator for FloorIterator<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.input.len() {
+            return None;
+        }
+        self.input
+            .get(self.index..)
+            .unwrap()
+            .chars()
+            .next()
+            .inspect(|_| self.index += 1)
+            .map(|c| {
+                self.floor += if c == '(' { 1 } else { -1 };
+                self.floor
+            })
+    }
+}
+
+fn part1(input: &str) -> i32 {
+    FloorIterator::new(input).last().unwrap()
+}
+
+fn part2(input: &str) -> usize {
+    FloorIterator::new(input)
+        .position(|floor| floor == -1)
+        .unwrap()
+        + 1
+}
+
+fn main() {
+    let input = fs::read_to_string("../input.txt").unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
