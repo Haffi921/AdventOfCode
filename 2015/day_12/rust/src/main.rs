c8b705f1590fdafcbeacc88e2f2ffc72de01a06c
Hafsteinn Ragnarsson <haffi921@gmail.com>
2024-12-01 21:04:58 +0100

    Day 12 of 2015 completed

---
+++ 2015/day_12/rust/src/main.rs
@@ -0,0 +1,34 @@
+use serde_json::Value;
+
+fn purge_red(json_value: &Value) -> Value {
+    match json_value {
+        Value::Object(obj) if obj.values().any(|v| v == "red") => Value::Null,
+        Value::Object(obj) => {
+            Value::Object(obj.iter().map(|(k, v)| (k.clone(), purge_red(v))).collect())
+        }
+        Value::Array(arr) => Value::Array(arr.iter().map(purge_red).collect()),
+        _ => json_value.clone(),
+    }
+}
+
+fn sum_numbers(input: &str) -> isize {
+    input
+        .split(|c: char| !c.is_ascii_digit() && c != '-')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<isize>().unwrap())
+        .sum()
+}
+
+fn part_1(input: &str) -> isize {
+    sum_numbers(input)
+}
+
+fn part_2(input: &str) -> isize {
+    sum_numbers(&serde_json::to_string(&purge_red(&serde_json::from_str(input).unwrap())).unwrap())
+}
+
+fn main() {
+    let input = include_str!("../../input.txt").trim();
+    println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
+}
